4e3f82479b7f93f44096ac8889d9fc12b142f7a1
bmbo123 <gregoryalan2005@gmail.com>
2023-04-02 16:48:31 -0600

    flappy

---
+++ flappy.py
@@ -0,0 +1,61 @@
+import pygame 
+from pygame import rect
+import random
+import time
+pygame.init()
+clock = pygame.time.Clock()
+
+Screen_Width = 1280
+Screen_Height = 720
+SCREEN = pygame.display.set_mode((Screen_Width, Screen_Height))
+pygame.display.set_caption("Fortnite")
+TUBE_COLOR = (150,250,100)
+ENEMY_COLOR = (250,0,100)
+SPEED = 5
+background = (10,20,30)
+GRAVITY = 10
+def playeranimation(player):
+    player.y += GRAVITY
+
+def tube_animation(arrtubes):
+    for tube in arrtubes:
+        tube.top.x -= SPEED
+        tube.bottom.x -= SPEED
+
+class Tube:
+    def  __init__(self):
+        mrandom =  random.randint(100,500)
+        self.top = pygame.Rect(1280,0,100,mrandom)
+        self.bottom = pygame.Rect(1280,200+ mrandom,100,600)   
+
+def rendertubes(arrtubes):
+    for tube in arrtubes:
+        pygame.draw.rect(SCREEN,TUBE_COLOR,tube.top)
+        pygame.draw.rect(SCREEN,TUBE_COLOR,tube.bottom)
+
+def controls():
+    
+    
+arrtubes = []
+
+time = 0
+player = pygame.Rect(1280/2, 720/2, 25,25)
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    SCREEN.fill(background)
+    currenttime = pygame.time.get_ticks()
+    if currenttime - time > 1700:
+        arrtubes.append(Tube())
+        time = currenttime
+    pygame.draw.rect(SCREEN,ENEMY_COLOR,player)
+    playeranimation(player)
+    rendertubes(arrtubes)
+    tube_animation(arrtubes)
+    pygame.display.update()
+    clock.tick(60)
+
+
+
